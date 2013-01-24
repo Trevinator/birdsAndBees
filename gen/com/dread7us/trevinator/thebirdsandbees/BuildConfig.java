c4364cf9623e79ffb3a6539b02b4570a76916b5a
Trevinator <isql@live.com>
2013-01-23 19:19:55 -0800

    this better work

---
+++ gen/com/dread7us/trevinator/thebirdsandbees/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dread7us.trevinator.thebirdsandbees;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
